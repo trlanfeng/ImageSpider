50d550f924eff53d8b0a64606d36938778faddca
孤月蓝风 <trlanfeng@foxmail.com>
2019-05-12 16:59:47 +0800

    花瓣第一版

---
+++ index.js
@@ -0,0 +1,59 @@
+const axios = require('axios');
+const fs = require('fs');
+
+let isFinish = false;
+const pageSize = 20;
+
+const testUrl = 'https://huaban.com/boards/46589133/';
+
+async function getPageData(url) {
+    const res = await axios.get(url, {
+        headers: {
+            'X-Request': 'JSON',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    });
+    const data = res.data;
+    const folderName = data.board.title;
+    const path = `images/${folderName}`;
+    const urlList = data.board.pins.map(item => item.file.key);
+    const pinsCount = data.board.pins.length;
+    fs.mkdir(path, (err) => {
+
+        if (err && err.code !== 'EEXIST') {
+            throw err;
+        }
+        getAndSaveImage(urlList, path);
+
+        if (pinsCount < pageSize) {
+            return;
+        } else {
+            max = data.board.pins[pinsCount - 1].pin_id;
+            url = `${testUrl}?max=${max}&limit=${pageSize}`;
+            getPageData(url);
+        }
+    });
+}
+
+function getAndSaveImage(list, path) {
+    list.forEach(item => {
+        axios.get(`https://hbimg.huabanimg.com/${item}`, {
+            responseType: "arraybuffer",
+        }).then(res => {
+            fs.writeFile(`${path}/${item}.jpg`, res.data, "binary", function (err) {
+                if (err) {
+                    console.log('TCL: main -> err', err);
+                } else {
+                    console.log(`${item}保存成功`);
+                }
+            });
+        })
+    })
+}
+
+async function main() {
+    const url = process.argv[2];
+    let data = await getPageData(url);
+}
+
+main();
